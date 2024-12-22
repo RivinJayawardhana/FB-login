2e6df6bf3649943599c0ecc107b6ebd150cfb115
Rivin Jayawardana <127378095+IT22061652@users.noreply.github.com>
2024-12-22 14:32:36 +0530

    backend update

--- api/routes/staff.route.js
+++ api/routes/staff.route.js
@@ -1,21 +1,11 @@
 import express from 'express';
-import { verifyToken } from '../utils/verifyUser.js';
-import { add,sendmail } from '../controllers/Staff.controller.js';
-import { getstaff } from '../controllers/Staff.controller.js';
-import { Delete } from '../controllers/Staff.controller.js';
-import { Getmember } from '../controllers/Staff.controller.js';
-import {updatestaff} from "../controllers/Staff.controller.js";
-import { usersignin } from '../controllers/Staff.controller.js';
+
+import { add } from '../controllers/Staff.controller.js';
 
 const router = express.Router();
 
 router.post('/add',add);
-router.post('/sendmail',sendmail);
-router.get('/get',getstaff);
-router.delete('/delete/:id',Delete);
-router.get('/getmember/:id',Getmember);
-router.put('/updatemember/:id',updatestaff);
-router.post('/login',usersignin);
+
 
 
 
